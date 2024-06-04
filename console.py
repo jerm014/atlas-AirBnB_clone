e38fbd2ba9e1c5fc451b68dad9855548405f8824
Jenna Jones <jljones121@gmail.com>
2024-06-04 22:19:54 +0000

    added doc strings

--- console.py
+++ console.py
@@ -7,18 +7,23 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
+        """quit cmd to leave program"""
         return True
 
     def do_EOF(self, arg):
+        """EOF cmd to leave program"""
         return True
 
     def emptyline(self):
+        """do nothing on empty input"""
         pass
 
     def do_help(self, arg):
+        """help information"""
         cmd.Cmd.do_help(self, arg)
 
     def default(self, line):
+        """handles invalid command"""
         if line == '':
             return
         else:
