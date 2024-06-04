2bb5f3b852a54ac9c7fc53f38767576526e88d12
Jenna Jones <jljones121@gmail.com>
2024-06-04 22:08:58 +0000

    console

--- console.py
+++ console.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 """this file contains code for the console"""
 
+import cmd
 
-class Console:
-    pass
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        return True
+
+    def do_EOF(self, arg):
+        return True
+
+    def emptyline(self):
+        pass
+
+    def do_help(self, arg):
+        cmd.Cmd.do_help(self, arg)
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
