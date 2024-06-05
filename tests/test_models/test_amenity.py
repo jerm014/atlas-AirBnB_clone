e91b5e522d6fceb07590209089ea3082f93ba62b
jerm014 <jeremy.mitts@atlasschool.com>
2024-06-05 03:51:15 +0000

    added import amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,6 +2,7 @@
 """this file contains code for testing the Amenity class"""
 import unittest
 from models.engine.file_storage import FileStorage
+from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
