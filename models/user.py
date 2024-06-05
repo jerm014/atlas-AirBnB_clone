7713e8625e33d115eb392041fe29a07b11b3501d
jerm014 <jeremy.mitts@atlasschool.com>
2024-06-05 02:37:52 +0000

    simplified User

--- models/user.py
+++ models/user.py
@@ -18,39 +18,8 @@ class User(BaseModel):
     which was declared unsably broken in 1996.
     """
 
-    __email = ""
-    __first_name = ""
-    __last_name = ""
-    __password = ""
-
-    @property
-    def email(self):
-        return self.__email
-
-    @property
-    def password(self):
-        return self.__password
-
-    @property
-    def first_name(self):
-        return self.__first_name
-
-    @property
-    def last_name(self):
-        return self.__last_name
-
-    @email.setter
-    def email(self, value):
-        self.__email = value
-
-    @password.setter
-    def password(self, value):
-        self.__password = value
-
-    @first_name.setter
-    def first_name(self,value):
-        self.__first_name = value
-
-    @last_name.setter
-    def last_name(self, value):
-        self.__last_name = value
+    email = ""
+    first_name = ""
+    last_name = ""
+    password = ""
+    
